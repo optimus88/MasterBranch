cdd9155d8475cd36219f8be237197fb00b3c2362
optimus88 <keshav_optimus88@outlook.com>
2016-12-21 11:46:57 +0530

    added the variableList file to the existing folder

---
+++ CM_Deployment_Final/variableList.sh
@@ -0,0 +1,24 @@
+#===== Begning of Declaration of Global Variable List ==================
+export AppDEVCM="UATRel_domain"
+export AppST_CM="ST_CM"
+export AppPERF="perfenv"
+export STDomain="BaNCS_ST_Build"
+export PerfDomain="Perfenv_Build"
+export DevCMDomain="Bancs_CMNEWDEV_BUILD"
+export initialPath=/home0/u234695/CM_DEPLOYMENT/
+export tempDest=/home0/u234695/CM_DEPLOYMENT/TempDest/
+export fromCopyPath=/home0/u234695/CM_DEPLOYMENT/DeskTemp/
+export backupPath=/home0/u234695/CM_DEPLOYMENT/BackUp_Files/
+export deployLog=/home0/u234695/CM_DEPLOYMENT/DeploymentLogs/DepLog/DeployedFiles.log
+export buildLog=/home0/u234695/CM_DEPLOYMENT/DeploymentLogs/BuildLogs/DeploymentBuild.log
+export deployDetailsFile=/home0/u234695/CM_DEPLOYMENT/DeployDetailsFile/pdnList.lst
+export buildNos=/home0/u234695/CM_DEPLOYMENT/DeployDetailsFile/buildNumber.lst
+export processedPDN=/home0/u234695/CM_DEPLOYMENT/ProcessedPDN/
+export defaultPath="/home0/u234695/.jenkins/jobs/"
+export workspacePath="/workspace/Base Source Code/ECLIPSEWORKSPACE/"
+export defaultAppPath="/home0/weblogic1035/Oracle/Middleware/user_projects/domains/"
+export appDeployPath="/applications/IIMS/"
+export scriptStatus="/home0/u234695/CM_DEPLOYMENT/tempLck/deploymentScript.lck"
+#$defaultAppPath"varName for the env"$appDeployPath
+
+#===== End of Declaration of Global Variable List ==================
